rock-core/drivers-orogen-transformer

commit 1080ec59e23e790a94bd9d397b5f8e380a8dab9f

Remove old-style access on Types: using Types.namespace.of.Type instead of Types::Namespace::Of::Type

rock/runtime.rb
@@ -44,7 +44,7 @@ module Transformer
         end
 
         def reset
-            @configuration_state = Types::Transformer::ConfigurationState.new
+            @configuration_state = Types.transformer.ConfigurationState.new
             @manager = Transformer::TransformationManager.new
         end
 
@@ -164,7 +164,7 @@ module Transformer
             end
 
             task.static_transformations = needed_static_transforms.each_value.map do |static|
-                rbs = Types::Base::Samples::RigidBodyState.invalid
+                rbs = Types.base.samples.RigidBodyState.invalid
                 rbs.sourceFrame = static.from
                 rbs.targetFrame = static.to
                 rbs.position = static.translation
@@ -187,7 +187,7 @@ module Transformer
             configuration_state.static_transformations =
                 manager.conf.
                     enum_for(:each_static_transform).map do |static|
-                        rbs = Types::Base::Samples::RigidBodyState.invalid
+                        rbs = Types.base.samples.RigidBodyState.invalid
                         rbs.sourceFrame = static.from
                         rbs.targetFrame = static.to
                         rbs.position = static.translation
@@ -206,7 +206,7 @@ module Transformer
                 begin
                     producer_name, producer_port_name = dyn.producer.split('.')
                     configuration_state.port_transformation_associations <<
-                        Types::Transformer::PortTransformationAssociation.new(:task => producer_name, :port => producer_port_name,
+                        Types.transformer.PortTransformationAssociation.new(:task => producer_name, :port => producer_port.name,
                                                                          :from_frame => dyn.from, :to_frame => dyn.to)
                 rescue Orocos::NotFound
                 end
@@ -216,13 +216,13 @@ module Transformer
                 task.each_input_port do |p|
                     if p.frame
                         configuration_state.port_frame_associations <<
-                            Types::Transformer::PortFrameAssociation.new(:task => task.name, :port => p.name, :frame => p.frame)
+                            Types.transformer.PortFrameAssociation.new(:task => task.name, :port => p.name, :frame => p.frame)
                     end
                 end
                 task.each_output_port do |p|
                     if p.frame
                         configuration_state.port_frame_associations <<
-                            Types::Transformer::PortFrameAssociation.new(:task => task.name, :port => p.name, :frame => p.frame)
+                            Types.transformer.PortFrameAssociation.new(:task => task.name, :port => p.name, :frame => p.frame)
                     end
                 end
             end

commit 998bb49bd5da704fbd4bd176dcad73b417664c35

Fix typo in variable name introduced in commit 1080ec5

rock/runtime.rb
@@ -207,7 +207,7 @@ module Transformer
                     producer_name, producer_port_name = dyn.producer.split('.')
                     configuration_state.port_transformation_associations <<
                         Types.transformer.PortTransformationAssociation.new(task: producer_name,
-                                                                            port: producer_port.name,
+                                                                            port: producer_port_name,
                                                                             from_frame: dyn.from,
                                                                             to_frame: dyn.to)
                 rescue Orocos::NotFound